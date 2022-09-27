34fde540d5dd93b20a2e3110eb8e9f6fe11c3b3e
Arthur Ramos <arthuuramos@gmail.com>
2022-09-27 20:59:01 -0300

    Condições exemplos e exercícios

---
+++ condicoes.js
@@ -0,0 +1,17 @@
+/* Executar usando a extensão do Node (F8)
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+if (vel > 60){
+    console.log('Você ultrapassou o limite da via. MULTADO!')
+} 
+console.log(`Dirija sempre com segurança!`)
+                                                                */
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}.`)
+if(pais == 'Brasil'){
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
